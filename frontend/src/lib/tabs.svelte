89c40a44325007a9d3858db4a611ddc7cc24fd9f
= <=>
2022-01-28 10:12:50 -0700

    update tabs css to be better looking?

--- frontend/src/lib/tabs.svelte
+++ frontend/src/lib/tabs.svelte
@@ -10,21 +10,19 @@
 </script>
 
 <!-- Tabs -->
-<ul id="tabs" class="inline-flex w-full px-1 pt-2 pb-5">
-  {#each router.routeList as tab}
-    <li
-      class="px-4 py-2 font-semibold rounded-t opacity-50 
-        {selected === tab ? 'border-b-4 border-blue-400' : ''}"
-    >
-      <button role="tab" data-href="#{tab.route}">{tab.label}</button>
-    </li>
-  {/each}
-</ul>
-
-<!-- Tab Contents -->
-<div
-  aria-live="polite"
-  class="border border-gray-500 overflow-x-auto h-full p-4 rounded-md"
->
-  <svelte:component this={selected.component} {...selected.params} />
+<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+  <ul id="tabs" class="flex w-full pt-2 pb-5 text-center">
+    {#each router.routeList as tab}
+      <li
+        class="py-2 w-full border-b-2
+        {selected === tab ? 'border-b-blue-400' : 'border-b-gray-200'}"
+      >
+        <button role="tab" data-href="#{tab.route}">{tab.label}</button>
+      </li>
+    {/each}
+  </ul>
+  <!-- Tab Contents -->
+  <div aria-live="polite" class="overflow-x-auto h-full p-4 rounded-md">
+    <svelte:component this={selected.component} {...selected.params} />
+  </div>
 </div>
