39e7cee25b80334b7a2144b851d747bdfda5d90a
= <=>
2022-01-29 12:20:39 -0700

    play around with adding transition to tab connect

--- frontend/src/lib/tabs.svelte
+++ frontend/src/lib/tabs.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import router from "src/lib/router";
+  import { fly } from "svelte/transition";
 
   export let tabs: any = [];
   router.addRoutes(tabs);
@@ -14,15 +15,21 @@
   <ul id="tabs" class="flex w-full pt-2 pb-5 text-center">
     {#each router.routeList as tab}
       <li
-        class="py-2 w-full border-b-2
-        {selected === tab ? 'border-b-blue-400' : 'border-b-gray-200'}"
+        class="py-2 w-full border border-b-2 border-gray-200 dark:border-gray-700
+        {selected === tab ? 'border-b-blue-400' : ''}"
       >
-        <button role="tab" data-href="#{tab.route}">{tab.label}</button>
+        <button role="tab" class="w-full" data-href="#{tab.route}"
+          >{tab.label}</button
+        >
       </li>
     {/each}
   </ul>
   <!-- Tab Contents -->
-  <div aria-live="polite" class="overflow-x-auto h-full p-4 rounded-md">
-    <svelte:component this={selected.component} {...selected.params} />
+  <div aria-live="polite" class="overflow-x-none h-full p-4 rounded-md">
+    {#key selected}
+      <div in:fly={{ x: 50 }}>
+        <svelte:component this={selected.component} {...selected.params} />
+      </div>
+    {/key}
   </div>
 </div>
